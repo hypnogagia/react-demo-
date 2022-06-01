ba90f5209e0fdbb687bc4884efbeb4d92204e116
Nikhil Kaushal <kaushal7509871736@gmail.com>
2022-06-01 08:00:30 +0530

    Hello.js created

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import Hello from "./Hello"
+
 const App = ()=>{
   const now = new Date()
   const a = 30
@@ -11,6 +13,8 @@ const App = ()=>{
     <p>
       {a} plus {b} is {a+b}
     </p>
+    <Hello name="el" planet="earth"/>
+    <Hello name="ed" planet="venus"/>
     </div>
   )
 }
---
+++ src/Hello.js
@@ -0,0 +1,11 @@
+const Hello =(props)=>{
+
+    return(
+        <div>
+            <p>hello {props.name}</p>
+            <p>Greetings from {props.planet}</p>
+        </div>
+    )
+}
+
+export default Hello
